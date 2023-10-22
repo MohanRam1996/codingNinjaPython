5c5492b2bc79d3ccdb51837a24b70a11f34d3cad
Mohan Ram <mohan.ram@influx.co.in>
2023-10-22 06:00:23 +0530

    initial commit

---
+++ Basics/palindromeOfNumber.py
@@ -0,0 +1,8 @@
+n = 121
+n = str(n)
+print(n==n[::-1])
+
+if True:
+    print("Pos")
+else:
+    print("Neg")
---
+++ DataTypes.py
@@ -0,0 +1,9 @@
+a = 10
+b= 11
+print(id(a))
+print(id(b))
+
+a = int(input())
+b = int(input())
+print(a+b)
+
